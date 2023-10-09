f27e272d0d6ebb0ddbaea4c7d1aa7fb4c6e0f0b6
Alexandra Chazaze <alexchazaze@gmail.com>
2023-10-09 06:14:58 -0400

    Final Project
    Signed-off-by: Alexandra Chazaze <alexchazaze@gmail.com>

--- final_project/index.js
+++ final_project/index.js
@@ -4,15 +4,42 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+}
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+app.use(express.json());
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    let token = req.session.authorization;
+    if(token) {
+        token = token['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "Customer not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "Customer not logged in"})
+     }
+ });
  
 const PORT =5000;
 
